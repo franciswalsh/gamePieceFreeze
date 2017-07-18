1539df59c1845d67cbfc0368a88a85de62e74061
Frank <francisw396@gmail.com>
2017-07-18 10:48:15 -0400

    should be the only commit. Gamepiece is moving effectively with bounds included

---
+++ src/com/company/GamePiece.java
@@ -0,0 +1,83 @@
+package com.company;
+
+/**
+ * Created by franciswalsh on 7/18/17.
+ */
+public class GamePiece {
+
+    int positionX;
+    int positionY;
+    int minX = 0;
+    int minY = 0;
+    int maxX = 100;
+    int maxY = 500;
+    boolean frozen;
+    String name;
+    String color;
+
+    public GamePiece(){
+        this.positionX = 0;
+        this.positionY = 0;
+        this.frozen = false;
+    }
+
+    public int getPositionX(){
+        return this.positionX;
+    }
+    public int getPositionY(){
+        return this.positionY;
+    }
+    public int getMinX(){
+        return this.minX;
+    }
+    public int getMinY(){
+        return this.minY;
+    }
+    public int getMaxX(){
+        return this.maxX;
+    }
+    public int getMaxY(){
+        return this.maxY;
+    }
+    public boolean getFrozen(){
+        return this.frozen;
+    }
+    public String getName(){
+        return this.name;
+    }
+    public String getColor(){
+        return this.color;
+    }
+    public void setName(String name){
+        this.name = name;
+    }
+    public void setColor(String color){
+        this.color = color;
+    }
+    public void move(int x, int y){
+
+        if (this.getFrozen()) {
+            System.out.println("Piece is frozen and cannot move to postion: (" + x + "," + y + ").");
+        }
+        else if (x > this.getMaxX() && y > this.getMaxY()){
+            System.out.println("Sorry, both x and y are  is outside of their respective bounds");
+        }
+        else if (x > this.getMaxX()) {
+            System.out.println("Sorry, " + x + " is outide of the x bound");
+        }
+        else if (y > this.getMaxY()) {
+            System.out.println("Sorry, " + y + " is outide of the y bound");
+        }
+        else {
+            this.positionX = x;
+            this.positionY = y;
+        }
+    }
+    public void freeze(){
+        this.frozen = true;
+    }
+    public void unfreeze(){
+        this.frozen = false;
+    }
+
+}
---
+++ src/com/company/Main.java
@@ -0,0 +1,49 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        GamePiece firstPiece = new GamePiece();
+
+        if (firstPiece.getFrozen() == false){
+            firstPiece.freeze();
+        }
+        else {
+            System.out.println("The game piece is not initially frozen");
+        }
+
+        if (firstPiece.getFrozen()){
+            firstPiece.unfreeze();
+        }
+        else {
+            System.out.println("The game piece was not frozen");
+        }
+
+        firstPiece.move(5, 7);
+
+        System.out.println("The new x position is: " + firstPiece.getPositionX() + ", and the new y position is: " + firstPiece.getPositionY() + ".");
+
+        firstPiece.freeze();
+
+        firstPiece.move(4, 3);
+
+        firstPiece.move(120, 420);
+
+        firstPiece.move(80, 520);
+
+        firstPiece.unfreeze();
+
+        firstPiece.move(100, 500);
+
+        System.out.println("New position x: " + firstPiece.getPositionX() + ". New position y: " + firstPiece.getPositionY() + ".");
+
+        firstPiece.move(100, 520);
+
+        firstPiece.move(120, 500);
+
+        System.out.println("New position x: " + firstPiece.getPositionX() + ". New position y: " + firstPiece.getPositionY() + ".");
+
+        firstPiece.move(120, 520);
+    }
+}
